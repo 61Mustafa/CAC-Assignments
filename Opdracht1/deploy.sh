b96243dea2e6bf3593e94a2e220146e99a1a9a6a
Mustafa Demir <demir61mustafa61@gmail.com>
2026-04-03 21:32:01 +0200

    Comments uit de bash scripts eruit gehaald.

--- Opdracht1/deploy.sh
+++ Opdracht1/deploy.sh
@@ -3,12 +3,12 @@
 # Stop het script direct als er een commando faalt
 set -e
 
-echo "🚀 Start met de uitrol van de CloudShirt Infrastructuur..."
+echo "Start met de uitrol van de Infrastructuur..."
 
 # ==========================================
 # 1. Fundament Stack (Netwerk & Security)
 # ==========================================
-echo "📦 1/4 Deploying Fundament Stack..."
+echo "1/4 Deploying Fundament Stack..."
 aws cloudformation deploy \
   --stack-name MyFundamentStack \
   --template-file 1_fundaments.yml
@@ -16,7 +16,7 @@ aws cloudformation deploy \
 # ==========================================
 # 2. Data & Storage Stack (RDS & EFS)
 # ==========================================
-echo "📦 2/4 Deploying Data & Storage Stack (Dit kan ~10-15 min duren ivm de Database)..."
+echo "2/4 Deploying Data & Storage Stack"
 aws cloudformation deploy \
   --stack-name MyStorageStack \
   --template-file 2_data\&storage.yml \
@@ -25,7 +25,7 @@ aws cloudformation deploy \
 # ==========================================
 # 3. Application Stack (Load Balancer & Launch Template)
 # ==========================================
-echo "📦 3/4 Deploying Application Stack..."
+echo "3/4 Deploying Application Stack..."
 aws cloudformation deploy \
   --stack-name MyAppStack \
   --template-file 3_application\&loadbalancer.yml \
@@ -34,16 +34,16 @@ aws cloudformation deploy \
 # ==========================================
 # 4. Auto Scaling Stack (ASG & Scheduled Actions)
 # ==========================================
-echo "📦 4/4 Deploying Auto Scaling Stack..."
+echo "4/4 Deploying Auto Scaling Stack..."
 aws cloudformation deploy \
   --stack-name MyASGStack \
   --template-file 4_auto_scaling.yml \
   --parameter-overrides BaseStackName=MyFundamentStack AppStackName=MyAppStack
 
-echo "✅ Alle stacks zijn succesvol uitgerold!"
+echo "Alle stacks zijn succesvol uitgerold!!!"
 
-# Haal de DNS naam (URL) van de Load Balancer op om direct te kunnen testen
+# Haal de DNS naam (URL) van de Load Balancer op
 ALB_URL=$(aws cloudformation describe-stacks --stack-name MyAppStack --query "Stacks[0].Outputs[?OutputKey=='LoadBalancerDnsName'].OutputValue" --output text)
 
-echo "🌐 Je applicatie is (zodra de server is opgestart) bereikbaar op:"
+echo "De applicatie is bereikbaar op:"
 echo "http://$ALB_URL"
