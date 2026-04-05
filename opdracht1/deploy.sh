4b07130753307d32506703e126ea94d1a257f88e
Mustafa Demir <demir61mustafa61@gmail.com>
2026-04-05 21:10:35 +0200

    ELK Stack cf staat klaar.

--- opdracht1/deploy.sh
+++ opdracht1/deploy.sh
@@ -8,7 +8,7 @@ echo "Start met de uitrol van de Infrastructuur..."
 # ==========================================
 # 1. Fundament Stack (Netwerk & Security)
 # ==========================================
-echo "1/4 Deploying Fundament Stack..."
+echo "1/5 Deploying Fundament Stack..."
 aws cloudformation deploy \
   --stack-name MyFundamentStack \
   --template-file 1_fundaments.yml
@@ -16,7 +16,7 @@ aws cloudformation deploy \
 # ==========================================
 # 2. Data & Storage Stack (RDS & EFS)
 # ==========================================
-echo "2/4 Deploying Data & Storage Stack"
+echo "2/5 Deploying Data & Storage Stack"
 aws cloudformation deploy \
   --stack-name MyStorageStack \
   --template-file 2_data\&storage.yml \
@@ -25,7 +25,7 @@ aws cloudformation deploy \
 # ==========================================
 # 3. Application Stack (Load Balancer & Launch Template)
 # ==========================================
-echo "3/4 Deploying Application Stack..."
+echo "3/5 Deploying Application Stack..."
 aws cloudformation deploy \
   --stack-name MyAppStack \
   --template-file 3_application\&loadbalancer.yml \
@@ -34,16 +34,27 @@ aws cloudformation deploy \
 # ==========================================
 # 4. Auto Scaling Stack (ASG & Scheduled Actions)
 # ==========================================
-echo "4/4 Deploying Auto Scaling Stack..."
+echo "4/5 Deploying Auto Scaling Stack..."
 aws cloudformation deploy \
   --stack-name MyASGStack \
   --template-file 4_auto_scaling.yml \
   --parameter-overrides BaseStackName=MyFundamentStack AppStackName=MyAppStack
 
+# ==========================================
+# 5. ELK Stack
+# ==========================================
+echo "5/5 Deploying Monitoring Stack..."
+aws cloudformation deploy \
+  --stack-name MyMonitoringStack \
+  --template-file 5_monitoring.yml \
+  --parameter-overrides BaseStackName=MyFundamentStack StorageStackName=MyStorageStack
+
 echo "Alle stacks zijn succesvol uitgerold!!!"
 
-# Haal de DNS naam (URL) van de Load Balancer op
+# Haal de DNS naam (URL) van de Load Balancer op en de die van Kibana
 ALB_URL=$(aws cloudformation describe-stacks --stack-name MyAppStack --query "Stacks[0].Outputs[?OutputKey=='LoadBalancerDnsName'].OutputValue" --output text)
+KIBANA_URL=$(aws cloudformation describe-stacks --stack-name MyMonitoringStack --query "Stacks[0].Outputs[?OutputKey=='KibanaUrl'].OutputValue" --output text)
 
 echo "De applicatie is bereikbaar op:"
 echo "http://$ALB_URL"
+echo "http://$KIBANA_URL"
--- opdracht1/destroy.sh
+++ opdracht1/destroy.sh
@@ -4,35 +4,43 @@ echo "Start met het verwijderen van de infrastructuur..."
 echo "Dit kan zo'n 10 tot 15 minuten duren (het verwijderen van de RDS database en de NAT Gateway kost tijd). Pak even een kopje koffie :)"
 
 # ==========================================
+# 0. Verwijder ELK Stack (Stack 5)
+# ==========================================
+echo "1/5 Verwijderen van Monitoring Stack (MyMonitoringStack)..."
+aws cloudformation delete-stack --stack-name MyMonitoringStack
+aws cloudformation wait stack-delete-complete --stack-name MyMonitoringStack
+echo "MonitoringStack verwijderd."
+
+# ==========================================
 # 1. Verwijder Auto Scaling (Stack 4)
 # ==========================================
-echo "1/4 Verwijderen van Auto Scaling Stack (MyASGStack)..."
+echo "2/5 Verwijderen van Auto Scaling Stack (MyASGStack)..."
 aws cloudformation delete-stack --stack-name MyASGStack
 aws cloudformation wait stack-delete-complete --stack-name MyASGStack
-echo "MyASGStack verwijderd."
+echo "ASGStack verwijderd."
 
 # ==========================================
 # 2. Verwijder Applicatie & ALB (Stack 3)
 # ==========================================
-echo "2/4 Verwijderen van Application Stack (MyAppStack)..."
+echo "3/5 Verwijderen van Application Stack (MyAppStack)..."
 aws cloudformation delete-stack --stack-name MyAppStack
 aws cloudformation wait stack-delete-complete --stack-name MyAppStack
-echo "MyAppStack verwijderd."
+echo "AppStack verwijderd."
 
 # ==========================================
 # 3. Verwijder Database & EFS (Stack 2)
 # ==========================================
-echo "3/4 Verwijderen van Data & Storage Stack (MyStorageStack)..."
+echo "4/5 Verwijderen van Data & Storage Stack (MyStorageStack)..."
 aws cloudformation delete-stack --stack-name MyStorageStack
 aws cloudformation wait stack-delete-complete --stack-name MyStorageStack
-echo "MyStorageStack verwijderd."
+echo "StorageStack verwijderd."
 
 # ==========================================
 # 4. Verwijder Fundament & Netwerk (Stack 1)
 # ==========================================
-echo "4/4 Verwijderen van Fundament Stack (MyFundamentStack)..."
+echo "5/5 Verwijderen van Fundament Stack (MyFundamentStack)..."
 aws cloudformation delete-stack --stack-name MyFundamentStack
 aws cloudformation wait stack-delete-complete --stack-name MyFundamentStack
-echo "MyFundamentStack verwijderd."
+echo "FundamentStack verwijderd."
 
 echo "Alles is platgegooid! De AWS omgeving is weer helemaal schoon en mijn credits zijn weer veilig :))."
